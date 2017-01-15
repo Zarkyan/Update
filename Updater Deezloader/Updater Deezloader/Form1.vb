1de88fd6304af5dac2b440835059f1e0c8e143f0
Zarkyan <akihikozarkyan@gmail.com>
2017-01-15 20:23:44 +0100

    Update

--- Updater Deezloader/Updater Deezloader/Form1.vb
+++ Updater Deezloader/Updater Deezloader/Form1.vb
@@ -5,6 +5,7 @@
         FlatLabel1.Text = "Vérification des mises à jour"
         FlatLabel2.Text = ""
         Timer1.Start()
+        Me.Show()
         VerifMaJ()
     End Sub
     Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
@@ -16,6 +17,7 @@
             FlatLabel1.Text = "Vérification des mises à jour..."
         ElseIf FlatLabel1.Text = "Vérification des mises à jour..." Then
             FlatLabel1.Text = "Vérification des mises à jour"
+
         End If
     End Sub
     Private Sub ProgressBarTimer_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ProgressBarTimer.Tick
@@ -28,34 +30,33 @@
 
     Sub VerifMaJ()
         'Premier lancement
-        If System.IO.File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Azuria\bin\Version_Updater.txt") = False Then
-            My.Computer.Network.DownloadFile("https://dl.dropboxusercontent.com/s/slooxpjg8oxe63j/Version_Updater.txt?token_hash=AAHJl5BZQL1PUSQ7fhpNKNfm4qR5sSEiKQgwCFEr7oJHKw&dl=1", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Azuria\bin\Version_Updater.txt")
-            My.Computer.Network.DownloadFile("https://dl.dropboxusercontent.com/s/ju3lvxg7i97ocvs/Azuria.exe?token_hash=AAEeqgl_ti9-P27D0FTScbsFcm_P4XWvfL38WUOn5N2VSg&dl=1", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Azuria\Azuria.exe")
+        'If System.IO.File.Exists(Environment.GetFolderPath("bin\Version_Updater.txt")) = False Then
+        If My.Computer.FileSystem.FileExists("Verif_Version.txt") = False Then
+            My.Computer.Network.DownloadFile("https://dl.dropboxusercontent.com/content_link/pzGaIP0onzEWvNPko6CKEsjNceo0pBAzKsgKpHdSlIiaC9wmjLGsUr2PY2cfQMf5/file?dl=1", "Verif_Version.txt")
             Close()
         Else
             'Mise à jour automatique
-            My.Computer.Network.DownloadFile("https://dl.dropboxusercontent.com/s/slooxpjg8oxe63j/Version_Updater.txt?token_hash=AAHJl5BZQL1PUSQ7fhpNKNfm4qR5sSEiKQgwCFEr7oJHKw&dl=1", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Azuria\Version_Updater.txt")
-            Dim vn As New System.IO.StreamReader(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Azuria\bin\Version_Updater.txt")
-            Dim vni As New System.IO.StreamReader(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Azuria\Version_Updater.txt")
+            My.Computer.Network.DownloadFile("http://raw.githubusercontent.com/Zarkyan/Update/master/Version_Updater.txt", "\Version_Updater.txt")
+            Dim vn As New System.IO.StreamReader("\Verif_Version.txt")
+            Dim vni As New System.IO.StreamReader("\Version_Updater.txt")
             Dim nk As String
             Dim nki As String
             nk = vn.ReadLine
             nki = vni.ReadLine
             vn.Close()
             vni.Close()
-            Kill(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Azuria\Version_Updater.txt")
+            Kill(Environment.GetFolderPath("\Version_Updater.txt"))
             If nk < nki Then
-                MsgBox("Mise à jour du launcher..", MsgBoxStyle.Information)
                 'Mise à jour
-                Kill(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Azuria\bin\Version_Updater.txt")
-                Kill(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Azuria\Azuria.exe")
-                My.Computer.Network.DownloadFile("https://dl.dropboxusercontent.com/s/wthiabqn2kll3zl/Version.txt?token_hash=AAE_x9aN908thAHd5Bh0p0pLen7w8ZnAkDl_LvJZgR15Ww&dl=1", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Azuria\bin\Version_Updater.txt")
-                My.Computer.Network.DownloadFile("https://dl.dropboxusercontent.com/s/ju3lvxg7i97ocvs/Azuria.exe?token_hash=AAEeqgl_ti9-P27D0FTScbsFcm_P4XWvfL38WUOn5N2VSg&dl=1", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Azuria\Azuria.exe")
+                Kill(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Verif_Version.txt")
+                My.Computer.Network.DownloadFile("https://raw.githubusercontent.com/Zarkyan/Update/master/Version_Updater.txt", "\Verif_Version.txt")
                 Close()
             End If
         End If
-        'Lancement de Azuria.exe
-        Process.Start(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Azuria\Azuria.exe")
+        'Lancement de DeezLoader
+        MsgBox("Yeah")
+        Process.Start("start.bat")
+        'Process.Start(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\DeezLoader\start.bat")
         Close()
     End Sub
 End Class
